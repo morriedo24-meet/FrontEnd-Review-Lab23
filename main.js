830a5004f0fea319f18d4ea91319acce50f659f3
morriedo24-meet <morriedoron@gmail.com>
2023-07-17 16:27:27 +0300

    finished lab

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function changeBackgroundColor(color){
-    document.body.style.backgroundColor = color;
-}
+var clickState = 0;
+function changeBackgroundColor(color, color2){
+	   
+	   clickState++;
+
+        if (clickState ==1 ) {
+            document.body.style.backgroundColor = color;
+        }if (clickState == 2 ) {
+             document.body.style.backgroundColor = color2;
+        } 
+        if(clickState==2){
+        	clickState = 0;
+        }
+		
+
+    }
+    
+  
+    
